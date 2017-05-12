a725cde274f573cb3ee4a7d1d3988ec137781864
JaxBBLL <showcye@foxmail>
2017-05-12 17:31:48 +0800

    #

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import 'element-react';
 import 'element-theme-default';
 import { Router, Route, hashHistory } from 'react-router';
 
+import './index.css';
 import Home from './Home';
 import ButtonComponent from './ButtonComponent';
 import TreeComponent from './TreeComponent';
+import TableComponent from './TableComponent';
 
 ReactDOM.render(
     <Router history={hashHistory} >   
@@ -16,6 +18,8 @@ ReactDOM.render(
         <Route path="/button" component={ButtonComponent} >
         </Route>
         <Route path="/tree" component={TreeComponent} >
+        </Route>
+        <Route path="/table" component={TableComponent} >
         </Route>
     </Router>
     , document.getElementById('root')
