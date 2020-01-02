900bc5a0e4a99239adb23e6a19801d4baa3ff46e
JaxBBLL <showcye@foxmail.com>
2020-01-02 13:04:56 +0800

    branch: gh-pages

---
+++ precache-manifest.e8554780b0e855994db98ff9685bf916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5963be5727f2a77436090c33085e621",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "2cc969fcc6e4a0213854",
+    "url": "/react-admin/static/css/2.33e7a59d.chunk.css"
+  },
+  {
+    "revision": "305f453c5e7df9f5b05a",
+    "url": "/react-admin/static/css/main.5e71bc84.chunk.css"
+  },
+  {
+    "revision": "2cc969fcc6e4a0213854",
+    "url": "/react-admin/static/js/2.12ef3332.chunk.js"
+  },
+  {
+    "revision": "305f453c5e7df9f5b05a",
+    "url": "/react-admin/static/js/main.4a72223d.chunk.js"
+  },
+  {
+    "revision": "cb40aa34c91cf90beb57",
+    "url": "/react-admin/static/js/runtime~main.8b0696f4.js"
+  }
+]);
